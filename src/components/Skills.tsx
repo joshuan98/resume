de14e75b08408fd1fee5737530306e0023ee7907
joshuan98 <89023959+joshuan98@users.noreply.github.com>
2024-10-10 23:35:39 +0800

    Remove utils folder

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,7 +1,34 @@
 import React from 'react';
 import skillsData from '../data/skills.json';
-import { getSkillLevelColor } from '../utils/skillsUtils';
 
+const getSkillLevelColor = (level: string) => {
+  switch (level) {
+    case 'proficient':
+      return {
+        bgText: 'bg-lime-500',
+        bgBar: 'bg-lime-700',
+        width: '80%',
+      };
+    case 'intermediate':
+      return {
+        bgText: 'bg-blue-500',
+        bgBar: 'bg-blue-700',
+        width: '50%',
+      };
+    case 'basic':
+      return {
+        bgText: 'bg-red-500',
+        bgBar: 'bg-red-700',
+        width: '30%',
+      };
+    default:
+      return {
+        bgText: 'bg-gray-500',
+        bgBar: 'bg-gray-700',
+        width: '0%',
+      };
+  }
+};
 
 const Skills: React.FC = () => {
   return (
