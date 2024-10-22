bc04ec8b2c9a2b40cc610dddeb647ca5aec3b7f4
joshuan98 <89023959+joshuan98@users.noreply.github.com>
2024-10-22 19:19:31 +0800

    Update color of skills bar and text

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -8,26 +8,26 @@ const getSkillLevelColor = (level: string) => {
   switch (level) {
     case 'proficient':
       return {
-        bgText: 'bg-lime-500',
-        bgBar: 'bg-lime-700',
+        bgText: 'bg-lime-600',
+        bgBar: 'bg-lime-800',
         width: '80%',
       };
     case 'intermediate':
       return {
-        bgText: 'bg-blue-500',
-        bgBar: 'bg-blue-700',
+        bgText: 'bg-blue-600',
+        bgBar: 'bg-blue-800',
         width: '50%',
       };
     case 'basic':
       return {
-        bgText: 'bg-red-500',
-        bgBar: 'bg-red-700',
+        bgText: 'bg-red-600',
+        bgBar: 'bg-red-800',
         width: '30%',
       };
     default:
       return {
-        bgText: 'bg-gray-500',
-        bgBar: 'bg-gray-700',
+        bgText: 'bg-gray-600',
+        bgBar: 'bg-gray-800',
         width: '0%',
       };
   }
@@ -44,7 +44,7 @@ const Skills: React.FC = () => {
           const { bgText, bgBar, width } = getSkillLevelColor(item.level);
           return (
             <div key={index} className="flex items-center">
-              <div className={`w-32 text-sm font-medium text-center text-primary p-2 rounded ${bgText}`}>
+              <div className={`w-32 text-sm font-medium text-center text-secondary p-2 rounded ${bgText}`}>
                 {item.name}
               </div>
               <div className="flex-1 ml-4 bg-gray-300 rounded-full h-4">
